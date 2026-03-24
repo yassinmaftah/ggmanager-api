fd0d1d220496dcb36c455831eba630bbff420fff
kara7z <kara.ossama12@gmail.com>
2026-03-24 15:05:00 +0100

    Migrations creation and models

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+
+            $table->enum('role', ['organizer', 'player'])->default('player');
+
             $table->timestamps();
         });
 
---
+++ database/migrations/2026_03_24_135153_create_tournaments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('tournaments', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('organizer_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+
+            $table->string('name');
+            $table->string('game');
+            $table->dateTime('date');
+
+            $table->integer('max_participants');
+
+            $table->string('format')->default('single_elimination');
+
+            $table->enum('status', [
+                'open',
+                'closed',
+                'in_progress',
+                'finished'
+            ])->default('open');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tournaments');
+    }
+};
