28207cebdb066b9995a2daabca6b3d55280ff5e5
kara7z <kara.ossama12@gmail.com>
2026-03-24 16:43:33 +0100

    installing sanctum and making Tournament controller

--- routes/api.php
+++ routes/api.php
@@ -3,19 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TournamentController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-
+Route::apiResource('tournament', TournamentController::class);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
 
-    Route::middleware('role:organizer')->group(function () {
-    });
+    Route::middleware('role:organizer')->group(function () {});
 
-    Route::middleware('role:player')->group(function () {
-    });
+    Route::middleware('role:player')->group(function () {});
 });
