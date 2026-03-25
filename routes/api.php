a7277addb000902ede0a90391dbb1af855ca25a1
zakariadachi <dachiziko@gmail.com>
2026-03-25 11:49:30 +0100

    ajouter MatchController avec sa logic metier

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TournamentController;
 use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\StatusController;
+use App\Http\Controllers\MatchController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -26,6 +27,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/tournaments/{tournament}/register', [RegistrationController::class, 'store']);
     Route::post('/tournaments/{tournament}/close', [StatusController::class, 'close']);
+    Route::post('/matches/{match}/score', [MatchController::class, 'submitScore']);
 });
 
 Route::get('/tournaments', [App\Http\Controllers\TournamentController::class, 'index']);
